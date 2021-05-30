4e6a1bca6f611ffbbbc8dbfff14291378dc98b2a
Sandhu <Sandhu@Sandhus-MacBook-Pro.local>
2021-05-30 22:25:56 +0530

    Telegram

--- utils/SendTelegramMessage.py
+++ utils/SendTelegramMessage.py
@@ -3,8 +3,6 @@ import telebot
 import requests
 import time
 import datetime
-import proj_constant_var as const
-
 telegram_id_list = ['464308445','506426930']
 send_pdf_token = '564398612:AAEXUIfrJVFHfBnxS4Uot0Ob5vDPN8Ws69I'
 
@@ -20,18 +18,23 @@ def sendTextTelegram(totalResponse, chatId):
         time.sleep(30)
         sendTextTelegram(totalResponse, chatId)
 
-def send_text_to_telegram(findings):
-    print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') +" : Sending Text to telegram Started"
-    for id in const.telegram_id_list:
-        for record in findings:
-            sendTextTelegram(record,id)
-    print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') +" : Sending Text to telegram Finished"
+# def send_text_to_telegram(findings):
+#     print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') +" : Sending Text to telegram Started"
+#     for id in const.telegram_id_list:
+#         for record in findings:
+#             sendTextTelegram(record,id)
+#     print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') +" : Sending Text to telegram Finished"
+#
 
+# def send_pdf_to_user():
+#     for id in telegram_id_list:
+#         token = send_pdf_token
+#         tb = telebot.TeleBot(token)
+#         #document = open(pdf_file,'rb')
+#
+#         tb.send_message(id, 'hi hi')
+#         #tb.send_document(id,document)
+#         print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " : File Sent to Telegram on ")
 
-def send_pdf_to_user(pdf_file):
-    for id in const.telegram_id_list:
-        token = const.send_pdf_token
-        tb = telebot.TeleBot(token)
-        document = open(pdf_file,'rb')
-        tb.send_document(id,document)
-        print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " : File Sent to Telegram on " +
+for id in telegram_id_list:
+    sendTextTelegram('HI HI HI', id)
