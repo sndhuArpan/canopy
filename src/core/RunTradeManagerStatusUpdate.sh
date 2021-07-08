acc60326047980c6699f5ea274cb5eff8741beca
Sandhu <Sandhu@Sandhus-MacBook-Pro.local>
2021-07-08 23:08:06 +0530

    RunTradeManagerUpdateStatus

---
+++ src/core/RunTradeManagerStatusUpdate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PROJECT_BASE_PATH='/home/ec2-user/canopy/canopy'
+source $PROJECT_BASE_PATH/env/bin/activate
+
+
+python3 $PROJECT_BASE_PATH/src/trademanager/RunTradeManagerStatusUpdate.py
---
+++ src/trademanager/RunTradeManagerStatusUpdate.py
@@ -0,0 +1,4 @@
+from src.trademanager.TradeManager import TradeManager
+
+if __name__ == '__main__':
+    TradeManager().update_trade_status()
